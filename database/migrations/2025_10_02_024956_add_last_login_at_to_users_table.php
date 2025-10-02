9f867a5a5741a07cb52f18d79d63bd850d89027a
joveey <jovisywl@gmail.com>
2025-10-02 11:03:30 +0700

    Add: Migration

---
+++ database/migrations/2025_10_02_024956_add_last_login_at_to_users_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Tambahkan kolom 'last_login_at' setelah kolom 'password'
+            $table->timestamp('last_login_at')->nullable()->after('password');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('last_login_at');
+        });
+    }
+};
