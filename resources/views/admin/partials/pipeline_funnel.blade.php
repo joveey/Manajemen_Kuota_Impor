3d82041a41f00b4c4131355625dbee89f8350a17
Annisa <annisafitriana38@gmail.com>
2025-11-25 14:51:11 +0700

    Edit UI dashbaord

--- resources/views/admin/partials/pipeline_funnel.blade.php
+++ resources/views/admin/partials/pipeline_funnel.blade.php
@@ -11,7 +11,7 @@
           ['label'=>'Mapped','value'=>$metrics['mapped'] ?? 0,'hint'=>'Models mapped','route'=>route('admin.mapping.mapped.page')],
           ['label'=>'Open PO','value'=>number_format($metrics['open_po_qty'] ?? 0),'hint'=>'Outstanding qty','route'=>route('admin.purchase-orders.index')],
           ['label'=>'In-Transit','value'=>number_format($metrics['in_transit_qty'] ?? 0),'hint'=>'PO Outstanding (excl. ACC)','route'=>route('admin.purchase-orders.index')],
-          ['label'=>'GR (Actual)','value'=>number_format($metrics['gr_qty'] ?? 0),'hint'=>'Received','route'=>\Illuminate\Support\Facades\Route::has('admin.imports.gr.index') ? route('admin.imports.gr.index') : '#'],
+          ['label'=>'GR (Actual)','value'=>number_format($metrics['gr_qty'] ?? 0),'hint'=>'Received (last 30 days)','route'=>\Illuminate\Support\Facades\Route::has('admin.imports.gr.index') ? route('admin.imports.gr.index') : '#'],
         ];
       @endphp
       @foreach($steps as $s)
